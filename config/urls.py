55e4a0f5d05787fda89738dc67f86458e2df0b2f
Aleksey Dzogiy <asdzog@mail.ru>
2024-02-28 21:55:28 +0400

    add main urls

--- config/urls.py
+++ config/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls', namespace='blog')),
+    path('clients/', include('blog.urls', namespace='clients')),
 ] + static(settings.STATIC_URL,
            document_root=settings.STATIC_ROOT) + static(
     settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
