a71845675f8a6d8eb50c05ef930f422b0a3a2034
asd <asdzog@mail.ru>
2024-03-11 02:44:18 +0400

    create model Client

--- clients/models.py
+++ clients/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'null': True, 'blank': True}
+
+
+class Client(models.Model):
+    """Client model"""
+    email = models.EmailField(max_length=100, unique=True, verbose_name='Почта')
+    full_name = models.CharField(verbose_name='ФИО')
+    comment = models.TextField(**NULLABLE, verbose_name='Комментарий')
+
+    def __str__(self):
+        return f'{self.full_name} ({self.email})'
+
+    class Meta:
+        verbose_name = 'Клиент'
+        verbose_name_plural = 'Клиенты'
