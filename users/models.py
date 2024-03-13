5b7de049a4e45e20312af876b840b91c93bc4ac7
asd <asdzog@mail.ru>
2024-03-13 20:48:17 +0400

    add model User

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from clients.models import NULLABLE
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='почта')
+
+    country = models.CharField(max_length=50, verbose_name='страна', **NULLABLE)
+    email_verified = models.BooleanField(default=False, verbose_name='email подтвержден')
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
