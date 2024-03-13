8f9cf8bbb7998bc223f5188ae08584efe465a087
asd <asdzog@mail.ru>
2024-03-13 20:51:01 +0400

    add homeview

--- mailings/views.py
+++ mailings/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
+from mailings.models import Message, Mailing, MailingLog
+
 
 # Create your views here.
+class HomeView(TemplateView):
+    model = Mailing
+    template_name = 'mailings/base.html'
+    extra_context = {
+        'title': 'index'
+    }
