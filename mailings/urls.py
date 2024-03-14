e37087029fea9d95a217a58d315b40f0064e4214
asd <asdzog@mail.ru>
2024-03-14 18:06:49 +0400

    add urls and views

--- mailings/urls.py
+++ mailings/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
-from mailings.views import HomeView
+from mailings.views import HomeView, MailingCreateView, MailingListView, MailingUpdateView, MailingDeleteView, \
+    MailingDetailView
 
 app_name = 'mailings'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('mailing_create/', MailingCreateView.as_view(), name='mailing_create'),
+    path('mailing_detail/', MailingDetailView.as_view(), name='mailing_create'),
+    path('mailing_list/', MailingListView.as_view(), name='mailing_list'),
+    path('mailing_update/', MailingUpdateView.as_view(), name='mailing_update'),
+    path('mailing_delete/', MailingDeleteView.as_view(), name='mailing_delete'),
 ]
-
