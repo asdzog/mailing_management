649bb81e583b0596d4038eeb1d8f706434631134
asd <asdzog@mail.ru>
2024-03-21 19:25:25 +0400

    add/edit CRUD for mailings (views, urls)

--- mailings/urls.py
+++ mailings/urls.py
@@ -1,15 +1,22 @@
 from django.urls import path
 
 from mailings.views import HomeView, MailingCreateView, MailingListView, MailingUpdateView, MailingDeleteView, \
-    MailingDetailView
+    MailingDetailView, MessageCreateView, MessageDetailView, MessageListView, MessageUpdateView, MessageDeleteView
 
 app_name = 'mailings'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('mailing_create/', MailingCreateView.as_view(), name='mailing_create'),
-    path('mailing_detail/', MailingDetailView.as_view(), name='mailing_detail'),
+    path('mailing_detail/<int:pk>/', MailingDetailView.as_view(), name='mailing_detail'),
     path('mailing_list/', MailingListView.as_view(), name='mailing_list'),
-    path('mailing_update/', MailingUpdateView.as_view(), name='mailing_update'),
-    path('mailing_delete/', MailingDeleteView.as_view(), name='mailing_delete'),
+    path('mailing_update/<int:pk>/', MailingUpdateView.as_view(), name='mailing_update'),
+    path('mailing_delete/<int:pk>/', MailingDeleteView.as_view(), name='mailing_delete'),
+
+    path('message_create/', MessageCreateView.as_view(), name='message_create'),
+    path('message_detail/<int:pk>/', MessageDetailView.as_view(), name='message_detail'),
+    path('message_list/', MessageListView.as_view(), name='message_list'),
+    path('message_update/<int:pk>/', MessageUpdateView.as_view(), name='message_update'),
+    path('message_delete/<int:pk>/', MessageDeleteView.as_view(), name='message_delete'),
+
 ]
--- mailings/views.py
+++ mailings/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
-from django.utils.text import slugify
+from django.urls import reverse_lazy
 from django.views.generic import TemplateView, CreateView, ListView, UpdateView, DeleteView, DetailView
 
-from mailings.forms import MailingForm
+from mailings.forms import MailingForm, MessageForm
 from mailings.models import Message, Mailing, MailingLog
 
 
@@ -11,13 +10,14 @@ class HomeView(TemplateView):
     model = Mailing
     template_name = 'mailings/home.html'
     extra_context = {
-        'title': 'index'
+        'title': 'Главная страница сервиса рассылок'
     }
 
 
 class MailingCreateView(CreateView):
     model = Mailing
     form_class = MailingForm
+    success_url = reverse_lazy('mailings:mailing_list')
     extra_context = {
         'title': 'Создание рассылки'
     }
@@ -25,7 +25,6 @@ class MailingCreateView(CreateView):
     def form_valid(self, form):
         if form.is_valid():
             new_mailing = form.save()
-            new_mailing.slug = slugify(new_mailing.title)
             new_mailing.save()
 
         return super().form_valid(form)
@@ -33,6 +32,9 @@ class MailingCreateView(CreateView):
 
 class MailingListView(ListView):
     model = Mailing
+    extra_context = {
+        'title': 'Рассылки'
+    }
 
     def get_queryset(self, *args, **kwargs):
         queryset = super().get_queryset(*args, **kwargs)
@@ -41,12 +43,71 @@ class MailingListView(ListView):
 
 
 class MailingUpdateView(UpdateView):
-    pass
+    model = Mailing
+    fields = '__all__'
+    success_url = reverse_lazy('mailings:mailing_list')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_mailing = form.save()
+            new_mailing.save()
+
+        return super().form_valid(form)
 
 
 class MailingDeleteView(DeleteView):
-    pass
+    model = Mailing
+    success_url = reverse_lazy('mailings:mailing_list')
 
 
 class MailingDetailView(DetailView):
-    pass
+    model = Mailing
+
+
+class MessageCreateView(CreateView):
+    model = Message
+    form_class = MessageForm
+    success_url = reverse_lazy('mailings:message_list')
+    extra_context = {
+        'title': 'Создание письма для рассылки'
+    }
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_email = form.save()
+            new_email.save()
+
+        return super().form_valid(form)
+
+
+class MessageListView(ListView):
+    model = Message
+    extra_context = {
+        'title': 'Письма'
+    }
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        return queryset
+
+
+class MessageUpdateView(UpdateView):
+    model = Message
+    fields = '__all__'
+    success_url = reverse_lazy('mailings:message_list')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_email = form.save()
+            new_email.save()
+
+        return super().form_valid(form)
+
+
+class MessageDeleteView(DeleteView):
+    model = Message
+    success_url = reverse_lazy('mailings:message_list')
+
+
+class MessageDetailView(DetailView):
+    model = Message
