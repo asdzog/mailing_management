0ce7abdb92750612d588aeb2f39f321e6088ef4c
asd <asdzog@mail.ru>
2024-03-26 08:55:22 +0400

    add model for blog objects to admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import Post
+
+
+@admin.register(Post)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = '__all__'
+    list_filter = ('title', 'content', 'views_count', 'date_published', 'is_active',)
+    
