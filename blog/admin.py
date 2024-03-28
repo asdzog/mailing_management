b7ef03bb2ebc7e0b9bea717ac9321b6935f26070
Aleksey <asdzog@mail.ru>
2024-03-29 01:38:25 +0400

    add blog, clients to admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -5,6 +5,6 @@ from blog.models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'content', 'views_count', 'date_published', 'is_active',)
+    list_display = ('title', 'content', 'date_published', 'is_active',)
     list_filter = ('title', 'content', 'views_count', 'date_published', 'is_active',)
     
